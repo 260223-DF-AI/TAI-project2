519e56ec4fe1986ef71a9df2a20637712df1d318
aziets399 <axz399@case.edu>
2026-03-21 11:27:19 -0500

    combined post to file conversion

--- src/api/app.py
+++ src/api/app.py
@@ -16,8 +16,11 @@
 # @router.get("/exception")
 # def get_exception():
 #     raise HTTPException(status_code=404, detail="Not found")
+import pandas as pd
 from fastapi import FastAPI
 from pathlib import Path
+
+from util.file_reader import do_everything
 
 app = FastAPI()
 
@@ -33,6 +36,21 @@ def post_root():
     
     data_folder = Path("../../data")
     files = [f for f in data_folder.iterdir() if f.is_file() and (f.name.endswith(".csv"))]
+
+    dfs = []
+    invalid_df = []
+
+    # sort through each file and validate
+    for file in files:
+        df, invalid = do_everything(file)
+        dfs.append(df)
+        invalid_df(invalid)
+
+    # combine all dataframes
+    df = pd.concat(dfs)
+
+    # convert to parquet file
+    df.to_parquet("new_data/file.parquet", engine = 'pyarrow')
 
     #returns list of files
     return files
